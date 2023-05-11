8b11385698bf4abbcc3032ba93a9363afd42df28
Milo-ffm <131757806+Milo-ffm@users.noreply.github.com>
2023-05-11 08:57:18 +0000

    save

---
+++ Übung.R
@@ -0,0 +1,22 @@
+#this is my first r script
+#we will learn data.table
+
+#load data.table package
+library(data.table)
+# install.packages("data.table")
+install.packages("data.table")
+# intall curl
+install.packages("curl")
+
+#read in the data
+#data is from  our class
+url <- "https://shorturl.at/eixVX"
+survey <- fread(url)
+ install jypiter for R
+#install.packages("IRkernel")
+#IRkernel::installspec()
+
+#q1 is about have you ever studied Regression before
+
+
+
